b566d02629d6b7193d295b0d56e66a8b76934cc8
eunkyo <eunkyo@eunkyoui-Mac-Studio.local>
2022-07-11 15:38:39 +0900

    triangles

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 main();
 
+function NormalizedToDevice( coord, axisSize )
+{
+   var halfAxisSize = axisSize / 2;
+
+   var deviceCoord  = ( coord + 1 ) * halfAxisSize;
+
+   return deviceCoord;
+}
+
+
+function DeviceToNormalize ( coord, axisSize )
+{
+   var halfAxisSize = axisSize / 2;
+
+   var normaliseCoord  = ( coord / halfAxisSize ) - 1;
+
+   return normaliseCoord;
+}
+
+
 function main()
 {
     const canvas = document.querySelector( "#glcanvas" );
@@ -14,24 +34,31 @@ function main()
 	}
 
 	var vertices = [
-	   0.0, 0.0, 0.0,
-       0.5, -0.5, 0.0,
-       1.0, 1.0, 0.0
+	   0.0,0.5,0.0,
+      -0.5, -0.5, 0.0,
+      0.5, -0.5, 0.0
     ];
+
+    var indices = [0, 1, 2];
 
     var vertex_buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices),gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
+    var Index_Buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+
     var vertCode =
     
-        'attribute vec3 coordinates;' +
+       'attribute vec3 coordinates;' +
 
 	    'void main(void)' +
 	    '{' +
 	       ' gl_Position = vec4(coordinates, 1.0);' +
-	       'gl_PointSize = 10.0;'+
 	    '}';
     var vertShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertShader, vertCode);
@@ -58,6 +85,8 @@ function main()
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
 
+    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+
     var coord = gl.getAttribLocation(shaderProgram, "coordinates" );
 
     gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
@@ -72,5 +101,5 @@ function main()
 
     gl.viewport( 0, 0, canvas.width, canvas.height);
 
-    gl.drawArrays(gl.POINTS, 0, 3);
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
 }
